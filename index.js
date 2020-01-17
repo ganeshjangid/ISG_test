8c54fa924ff17799e674e2b7b4f2558ab7040932
ganesh suthar <webdeveloper.ganesh@gmail.com>
2020-01-17 06:56:48 +0000

    test

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
+const {CacheService} =require('./cahe_services');
+//console.log(CacheService);
 
-const NodeCache=require('node-cache');
-class Cache{
-  constructor(ttlSeconds){
-   this.cache= new NodeCache({stdTTL:ttlSeconds,checkperiod:ttlSeconds*0.2,useClones:false});
-  }
+const ttl = 60 * 60 * 1; // cache for 1 Hour
+const cache=new CacheService(ttl);
 
-  get(key,storeFunction){
-    const value=this.cache.get(key);
-    if (value) {
-      return Promise.resolve(value);
-    }
+const FeedModel = {
 
-    return storeFunction().then((result)=>{
-      this.cache.set(key,result);
+  getUserFeeds(userID) {
+    const selectQuery = `SELECT * FROM feeds WHERE userID = ${userID}`;
+    const key = `getUserFeeds_${userID}`;
+
+    return cache.get(key, () => DB.then((connection) =>
+      connection.query(selectQuery).then((rows) => {
+        return rows;
+      })
+    )).then((result) => {
       return result;
     });
+  },
+
+  getFeedById(feedID) {
+    const selectQuery = `SELECT * FROM feeds WHERE feedID = ${feedID}`;
+    const key = `getFeedById_${feedID}`;
+
+    return cache.get(key, () => DB.then((connection) =>
+      connection.query(selectQuery).then((rows) => {
+        return rows[0];
+      })
+    )).then((result) => {
+      return result;
+    });
+  },
+
+  countFeeds(userID) {
+    const selectQuery = `SELECT COUNT(*) as count FROM feeds WHERE userID = ${userID}`;
+    const key = `countFeeds_${userID}`;
+
+    return cache.get(key, () => DB.then((connection) =>
+      connection.query(selectQuery).then((rows) => {
+        return rows[0].count;
+      })
+    )).then((result) => {
+      return result;
+    });
+  },
+
+  update(userID, feedID, feedData) {
+    const whereQuery = `WHERE feedID = ${feedID} AND userID = ${userID}`;
+    const updateQuery = `UPDATE feeds SET ? ${whereQuery}`;
+
+    return DB.then((connection) => connection.query(updateQuery, feedData).then(() => {
+      cache.del([`getFeedById_${feedID}`, `getUserFeeds_${userID}`]);
+      return true;
+    }));
+  },
+
+  delete(userID, feedID) {
+    const deleteQuery = `DELETE from feeds WHERE userID = ${userID} AND feedID = ${feedID}`;
+    return DB.then((connection) => connection.query(updateQuery, params).then(() => {
+      cache.del([`countFeeds_${userID}`, `getFeedById_${feedID}`, `getUserFeeds_${userID}`]);
+      return true;
+    }));
   }
+};
 
-  del(keys) {
-    this.cache.del(keys);
-  }
-
-   delStartWith(startStr = '') {
-    if (!startStr) {
-      return;
-    }
-
-    const keys = this.cache.keys();
-    for (const key of keys) {
-      if (key.indexOf(startStr) === 0) {
-        this.del(key);
-      }
-    }
-  }
-
-  flush() {
-    this.cache.flushAll();
-  }
-
+module.exports={
+  FeedModel:FeedModel
 }
 
-exports.module=Cache;
